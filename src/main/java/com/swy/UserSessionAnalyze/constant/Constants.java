974f18d4e9503c2623f84e70351d61db6f0c7bfb
suwanyan <suwanyan311@163.com>
2018-08-28 20:08:52 +0800

    1. Select session by condition 2. Calculate the session proportion of the access time in various ranges of 1s~3s, 4s~6s, 7s~9s, 10s~30s, 30s~60s, 1m~3m, 3m~10m, 10m~30m, and 30m.The session proportion of the visiting step size in the range of 1~3, 4~6, 7~9, 10~30, 30~60 and above 3. In the session that meets the requirements, 1000 sessions are randomly selected according to the time proportion 4. Get the top 10 categories of click, order and payment quantity in the session meeting 5. For the top 10 categories, get the session with the top 10 click times

---
+++ src/main/java/com/swy/UserSessionAnalyze/constant/Constants.java
@@ -0,0 +1,17 @@
+package com.swy.UserSessionAnalyze.constant;
+
+/**
+ * 常量接口
+ * @author Administrator
+ *
+ */
+public interface Constants {
+
+	/**
+	 * 项目配置相关的常量
+	 */
+	String SPARK_LOCAL = "spark.local";
+	
+
+	
+}
